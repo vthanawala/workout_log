abc5e7a13500711cda0f751d82dfa64b987227f5
Raghu Betina <raghu@firstdraft.com>
2019-12-11 21:22:38 +0000

    Only workout user can access show page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,5 @@
 class WorkoutsController < ApplicationController
-  before_action :current_user_must_be_workout_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_workout_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_workout_user
     workout = Workout.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
