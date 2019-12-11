050a22ea3d474b005d2bd4824702b7eeabbc0ba0
Raghu Betina <raghu@firstdraft.com>
2019-12-11 21:22:37 +0000

    Only workout user can modify workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,14 @@
 class WorkoutsController < ApplicationController
+  before_action :current_user_must_be_workout_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_workout_user
+    workout = Workout.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == workout.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @workouts = Workout.all
 
