0d8bf31432749524db26ef6c36f66988157bee73
Raghu Betina <raghu@firstdraft.com>
2019-12-11 21:22:31 +0000

    Whitelist columns in Lift for ActiveAdmin

--- app/admin/lifts.rb
+++ app/admin/lifts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Lift do
+
+ permit_params :workout_id, :body_part, :comments
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
